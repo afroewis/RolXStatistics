a1be3a6320662d16ae852ace944745f73f3fde70
Andreas Fröwis <andreas.froewis@gmail.com>
2024-03-16 10:07:23 +0100

    Run function only on MO-FR

--- function_app.py
+++ function_app.py
@@ -8,7 +8,7 @@ from employees import employees, BUs
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 0 12 * * *", arg_name="myTimer", run_on_startup=True,
+@app.schedule(schedule="0 0 12 * * 1-5", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def publish_rolx_stats(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
